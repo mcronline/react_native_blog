acf9193eeaf2b8e10fbcb91957700c30e378ab6c
Michel Rozatti <contact@michelcurti.dev>
2019-12-17 17:11:02 -0300

    Basic Structure

---
+++ src/screens/ArticleScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ArticleScreen = () => {
+
+    return(
+        <View>
+            <Text>Show Article</Text>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+
+});
+
+export default ArticleScreen;
