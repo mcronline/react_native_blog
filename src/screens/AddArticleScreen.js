085d86485ac4d4f984565bdfc9938800fda7c433
Michel Rozatti <contact@michelcurti.dev>
2019-12-17 17:08:53 -0300

    Basic Structure

---
+++ src/screens/AddArticleScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import ArticleForm from '../components/ArticleForm';
+
+const AddArticleScreen = () => {
+
+    return(
+        <View>
+            <ArticleForm />
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+
+});
+
+export default AddArticleScreen;
---
+++ src/screens/EditArticleScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import ArticleForm from '../components/ArticleForm';
+
+const EditArticleScreen = () => {
+
+    return(
+        <ArticleForm />
+    );
+}
+
+const style = StyleSheet.create({
+
+});
+
+export default EditArticleScreen;
