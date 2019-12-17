98dfb2a15417738088ca31c30adf4b191ff0c8de
Michel Rozatti <contact@michelcurti.dev>
2019-12-17 20:58:51 -0300

    created redux store

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './src/reducers';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
@@ -12,4 +15,10 @@ const navigation = createStackNavigator({
     }
 })
 
-export default createAppContainer(navigation);
+const store = createStore(reducer);
+
+const Navigation = createAppContainer(navigation);
+
+export default <Provider store={store}>
+    <Navigation />
+</Provider>
