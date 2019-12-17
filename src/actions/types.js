f089f5c6e09adc4993cc136d09b161988ef77302
Michel Rozatti <contact@michelcurti.dev>
2019-12-17 17:18:55 -0300

    Define actions creators names

---
+++ src/actions/types.js
@@ -0,0 +1,4 @@
+export const ADD_ARTICLE = 'ADD_ARTICLE';
+export const EDIT_ARTICLE = 'EDIT_ARTICLE';
+export const FETCH_ARTICLES = 'FETCH_ARTICLES';
+export const FETCH_ARTICLE = 'FETCH_ARTICLE';
