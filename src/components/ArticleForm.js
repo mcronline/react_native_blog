d7ec72865edb9fd1a8b58aea85b159b5f08ca0f3
Michel Rozatti <contact@michelcurti.dev>
2019-12-18 11:35:40 -0300

    styling

--- src/components/ArticleForm.js
+++ src/components/ArticleForm.js
@@ -1,35 +1,55 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 const ArticleForm = (props) => {
 
     const { title, setTitle } = useState(props.title);
     const { content, setContent } = useState(props.content);
+    const dispatch = useDispatch();
 
     return(
-        <View>
-            <Text>Title</Text>
-            <TextInput
-                value={title}
-                placeholder="Title"
-                onChangeText={setTitle}
-            />
-            <Text>Content</Text>
-            <TextInput
-                value={content}
-                placeholder="Content"
-                onChangeText={setContent}
-            />
+        <View style={style.inputArea}>
+            <View>
+                <Text style={style.label}>Title</Text>
+                <TextInput
+                    style={style.input}
+                    value={title}
+                    placeholder="Title"
+                    onChangeText={setTitle}
+                />
+                <Text style={style.label}>Content</Text>
+                <TextInput
+                    style={style.input}
+                    value={content}
+                    placeholder="Content"
+                    onChangeText={setContent}
+                />
+            </View>
             <Button
                 title="Save Article"
-                onPress={onSubmit}
+                onPress={() => dispatch(onSubmit({title,content}))}
             />
         </View>
     );
 }
 
 const style = StyleSheet.create({
-
+    inputArea : {
+        padding : 15
+    },
+    label : {
+        fontSize : 20,
+        fontWeight : 'bold'
+    },
+    input : {
+        fontSize : 20,
+        marginBottom : 15,
+        borderColor : "#555",
+        borderWidth : 1,
+        paddingLeft : 5,
+        height : 40
+    }
 });
 
 export default ArticleForm;
